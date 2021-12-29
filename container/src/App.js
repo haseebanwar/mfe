1e5f316628c4ff0def758124b52bfafc95e5b411
Haseeb Anwar <haseebanwar99@gmail.com>
2021-12-29 12:19:08 +0500

    Prefixed container and marketing apps classnames to avoid css conflicts

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Header from './Header';
 import MarketingApp from './MarketingApp';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co', // prefix classnames with 'co' in production
+});
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </Router>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './pages/Landing';
 import Pricing from './pages/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma', // prefix classnames with 'ma' in production
+});
+
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route path="/" component={Landing} exact />
